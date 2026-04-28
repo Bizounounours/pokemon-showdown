87db7a7b63abf06c46b28e47eaeaa508c392ae04
Bizounounours <147722573+Bizounounours@users.noreply.github.com>
2026-04-28 16:30:07 +0200

    Add files via upload

---
+++ 35playermons/formats-data.ts
@@ -0,0 +1,242 @@
+export const FormatsData: { [k: string]: any } = {
+	psychictongues: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	ironsniper: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	grasswarrior: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	ironmotherfucker: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	mistereruption: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	ironprophet: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	threebans: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	thundergames: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	angrythunder: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	landoheimer: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	eamplsl: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	carnivore: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	shoota: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	xungo: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	chemfruit: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	nutshell: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	typhon: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	lily: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	hogwobblin: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	mcbruh: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	alice: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	shifty: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	riki: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	copper: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	jay: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	kizaco: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	rules: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	keboblepopper: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	chickenburger: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	washedmaster: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	apu: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	stan: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	beesus: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	cruize: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	bizou: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	portobello: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	lucy: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	cringecat: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	trippy: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	quesadilla: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	regilover: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	donovan: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	mrtrippy: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	sammy: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	maszt: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	friend: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	bonsoir: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	sluice: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	turtle: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	lpt: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	paudhury: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	zcarlett: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	tomotaco: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	msousa: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	belluno: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	sxitty: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	presi: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	gc: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	kiera: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+	jt: {
+		tier: "OU",
+		isNonstandard: null,
+	},
+};
